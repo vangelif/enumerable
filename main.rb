8638466b37195f89c25b2350053cec05a0c09b06
Vangelis <ev.fotiades@yahoo.com>
2023-11-14 00:41:34 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
 require_relative 'list'
 
-# Create our list
+# STEP FOUR: INSTANTIATION AND TESTING
+# create list object
 list = MyList.new(1, 2, 3, 4)
 
-# Test #all?
+# checks if elements are less or greater than 5
 puts(list.all? { |e| e < 5 }) # true
 puts(list.all? { |e| e > 5 }) # false
 
-# Test #any?
+# checks if elements equals to 2 or 5
 puts(list.any? { |e| e == 2 }) # true
 puts(list.any? { |e| e == 5 }) # false
 
-# Test #filter
+# filters elements based on the block condition (&:even?) 
 puts(list.filter(&:even?)) # [2, 4]
