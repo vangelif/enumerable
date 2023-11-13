3c461a130e06af562d21d38ad84535dfb6ae0dab
Vangelis <ev.fotiades@yahoo.com>
2023-11-14 00:17:34 +0800

    Update list.rb

--- list.rb
+++ list.rb
@@ -2,11 +2,15 @@ require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
-
+  
+# initialize method takes a variable number of elements
+# it then stores them in @list  
   def initialize(*elements)
     @list = elements
   end
 
+# each method iterates over the elements of the @list  
+# &block is a block parameter, and converts to a proc upon calling of each method - see documentation  
   def each(&block)
     @list.each(&block)
   end
